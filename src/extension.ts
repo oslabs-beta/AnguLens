8d23fe693cdeb67c2581fb60614cd9943eda2c6a
Simon Chadsey <simonchadsey@gmail.com>
2023-11-20 15:30:58 -0800

    removed hello world, moved TypeScript from devDependencies to Dependencies, added start angulens to activation events

--- src/extension.ts
+++ src/extension.ts
@@ -28,14 +28,6 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "angulens.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from AnguLens!");
-    }
-  );
 
   // Register the command for opening files in a new tab
   const openFileDisposable = vscode.commands.registerCommand(
@@ -238,7 +230,7 @@ export function activate(context: vscode.ExtensionContext) {
     });
   });
 
-  context.subscriptions.push(disposable, runWebView);
+  context.subscriptions.push(runWebView);
 }
 
 function getWebViewContent(
